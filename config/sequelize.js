389cdaa63289120e8e972b21faf5e3a845018978
Lucas Castro <lucas.castro@dex360.net>
2026-04-01 14:02:44 -0400

    Actualizar coneccion sequelice

--- config/sequelize.js
+++ config/sequelize.js
@@ -3,6 +3,8 @@ const process = require('process');
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config.js')[env];
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = config.url
+  ? new Sequelize(config.url, config)
+  : new Sequelize(config.database, config.username, config.password, config);
 
 module.exports = sequelize;
